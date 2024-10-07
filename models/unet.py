86d42bd1cd89dac4e08a88f4e240c57544e96ebd
aryamanpandya99 <aryaman.pandya@tufts.edu>
2024-10-06 21:28:07 -0400

    switched from conv2d to resblock based convolution blocks in the unet

--- models/unet.py
+++ models/unet.py
@@ -1,7 +1,21 @@
 import torch
 import torch.nn as nn
 
-from .resnet import ConvBlock
+from .resnet import ResBlock
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, num_layers):
+        super(ConvBlock, self).__init__()
+        self.conv1 = ResBlock(in_channels, out_channels)
+        
+        for i in range(num_layers-1):
+            self.conv1.append(ResBlock(out_channels, out_channels))
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        return x
 
 
 class DownBlock(nn.Module):
@@ -9,19 +23,19 @@ class DownBlock(nn.Module):
     Downampling (left) side of the UNet.
     Excludes the bottom-most conv block.
     """
-    def __init__(self, filters, in_channels):
+    def __init__(self, filters, in_channels, num_layers):
         super(DownBlock, self).__init__()
-        conv_blocks = [ConvBlock(in_channels, filters[0])]
+        res_blocks = [ResBlock(in_channels, filters[0])]
         for i in range(1, len(filters)):
-            conv_blocks.append(ConvBlock(filters[i-1], filters[i]))
+            res_blocks.append(ResBlock(filters[i-1], filters[i]))
 
-        self.conv_blocks = nn.Sequential(*conv_blocks)
+        self.res_blocks = nn.Sequential(*res_blocks)
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
     def forward(self, x):
         residual_outputs = []
-        for conv_block in self.conv_blocks:
-            x = conv_block(x)
+        for res_block in self.res_blocks:
+            x = res_block(x)
             residual_outputs.append(x)
             x = self.maxpool(x)
 
@@ -37,12 +51,12 @@ class UpBlock(nn.Module):
         for i in range(len(filters) - 2):
             layers.append(
                 nn.Sequential(
-                    ConvBlock(filters[i], filters[i+1]), 
+                    ResBlock(filters[i], filters[i+1]), 
                     nn.ConvTranspose2d(filters[i+1], filters[i+1]//2, 2, stride=2)
                 )
             )
         
-        layers.append(ConvBlock(filters[-2], filters[-1]))
+        layers.append(ResBlock(filters[-2], filters[-1]))
         self.layers = nn.Sequential(*layers)
     
     def forward(self, x, residual_outputs):
@@ -68,7 +82,7 @@ class UNet(nn.Module):
         # the bottom-most conv block is different from the rest of the blocks
         # in that it doesn't contain a maxpool and upsamples without a residual connection
         self.bottom_conv = nn.Sequential(
-            ConvBlock(down_filters[-1], down_filters[-1]*2), 
+            ResBlock(down_filters[-1], down_filters[-1]*2), 
             nn.ConvTranspose2d(down_filters[-1]*2, down_filters[-1], 2, stride=2)
         )
         
