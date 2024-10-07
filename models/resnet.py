e90fb9c623ed02792a6437221a84e1b7c97d32f8
aryamanpandya99 <aryaman.pandya@tufts.edu>
2024-10-06 21:29:31 -0400

    ruff

--- models/resnet.py
+++ models/resnet.py
@@ -2,43 +2,49 @@ import torch.nn as nn
 
 
 class ResBlock(nn.Module):
-    def __init__(self, 
-                 in_channels: int, 
-                 out_channels: int, 
-                 activation: nn.Module = nn.ReLU, 
-                 kernel_size: int = 3, 
-                 stride: int = 1, 
-                 padding: int = 1, 
-                 dropout: float = 0.2,
-                 num_groups: int = 1
-                 ):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        activation: nn.Module = nn.ReLU,
+        kernel_size: int = 3,
+        stride: int = 1,
+        padding: int = 1,
+        dropout: float = 0.2,
+        num_groups: int = 1,
+    ):
         super().__init__()
         self.conv = nn.Sequential(
             nn.GroupNorm(num_groups=num_groups, num_channels=in_channels),
             activation(),
-            nn.Conv2d(in_channels,
-                      out_channels,
-                      kernel_size=kernel_size,
-                      stride=stride,
-                      padding=padding), 
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+            ),
             activation(),
-            nn.Conv2d(out_channels,
-                      out_channels,
-                      kernel_size=kernel_size,
-                      stride=1,
-                      padding=padding),
+            nn.Conv2d(
+                out_channels,
+                out_channels,
+                kernel_size=kernel_size,
+                stride=1,
+                padding=padding,
+            ),
             nn.GroupNorm(num_groups=num_groups, num_channels=out_channels),
         )
-        
+
         if in_channels == out_channels:
             self.idconv = nn.Identity()
         else:
             self.idconv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1)
-        
-        self.avgpool = nn.Identity() if stride == 1 else nn.AvgPool2d(kernel_size=2, stride=stride)
+
+        self.avgpool = (
+            nn.Identity() if stride == 1 else nn.AvgPool2d(kernel_size=2, stride=stride)
+        )
         self.activation = activation()
         self.dropout = nn.Dropout(dropout)
-
 
     def forward(self, x):
         h = self.conv(x)
@@ -46,23 +52,27 @@ class ResBlock(nn.Module):
         return self.dropout(self.activation(h + h_id))
 
 
-
 class ResNet(nn.Module):
-    def __init__(self,
-                 num_channels: int,
-                 num_classes: int,
-                 filters: list[int],
-                 activation: nn.Module = nn.ReLU,
-                 stride: int = 2,
-                 dropout: float = 0.2
-                 ):
+    def __init__(
+        self,
+        num_channels: int,
+        num_classes: int,
+        filters: list[int],
+        activation: nn.Module = nn.ReLU,
+        stride: int = 2,
+        dropout: float = 0.2,
+    ):
         super().__init__()
-        res_layers = [
-            ResBlock(num_channels, filters[0], activation, stride=1)
-        ]
-        for i in range(len(filters)-1):
+        res_layers = [ResBlock(num_channels, filters[0], activation, stride=1)]
+        for i in range(len(filters) - 1):
             res_layers += [
-                ResBlock(filters[i], filters[i+1], activation, stride=stride, dropout=dropout)
+                ResBlock(
+                    filters[i],
+                    filters[i + 1],
+                    activation,
+                    stride=stride,
+                    dropout=dropout,
+                )
             ]
         self.res_layers = nn.Sequential(*res_layers)
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
@@ -70,9 +80,10 @@ class ResNet(nn.Module):
         self.linear = nn.Sequential(
             nn.Linear(filters[-1], num_classes, bias=False),
             nn.BatchNorm1d(num_classes),
-            activation()
+            activation(),
         )
         self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         for _, layer in enumerate(self.res_layers):
             x = layer(x)
--- models/unet.py
+++ models/unet.py
@@ -8,8 +8,8 @@ class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels, num_layers):
         super(ConvBlock, self).__init__()
         self.conv1 = ResBlock(in_channels, out_channels)
-        
-        for i in range(num_layers-1):
+
+        for i in range(num_layers - 1):
             self.conv1.append(ResBlock(out_channels, out_channels))
 
     def forward(self, x):
@@ -23,11 +23,12 @@ class DownBlock(nn.Module):
     Downampling (left) side of the UNet.
     Excludes the bottom-most conv block.
     """
+
     def __init__(self, filters, in_channels, num_layers):
         super(DownBlock, self).__init__()
         res_blocks = [ResBlock(in_channels, filters[0])]
         for i in range(1, len(filters)):
-            res_blocks.append(ResBlock(filters[i-1], filters[i]))
+            res_blocks.append(ResBlock(filters[i - 1], filters[i]))
 
         self.res_blocks = nn.Sequential(*res_blocks)
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -41,35 +42,39 @@ class DownBlock(nn.Module):
 
         return residual_outputs, x
 
+
 class UpBlock(nn.Module):
     """
     Upsampling (right) side of the UNet.
     """
+
     def __init__(self, filters):
         super(UpBlock, self).__init__()
         layers = []
         for i in range(len(filters) - 2):
             layers.append(
                 nn.Sequential(
-                    ResBlock(filters[i], filters[i+1]), 
-                    nn.ConvTranspose2d(filters[i+1], filters[i+1]//2, 2, stride=2)
+                    ResBlock(filters[i], filters[i + 1]),
+                    nn.ConvTranspose2d(
+                        filters[i + 1], filters[i + 1] // 2, 2, stride=2
+                    ),
                 )
             )
-        
+
         layers.append(ResBlock(filters[-2], filters[-1]))
         self.layers = nn.Sequential(*layers)
-    
+
     def forward(self, x, residual_outputs):
         for i in range(len(self.layers)):
             print(f"i: {i}")
-            residual = residual_outputs[-(i+1)]
+            residual = residual_outputs[-(i + 1)]
             _, _, h, w = x.shape
             residual = residual[:, :, :h, :w]
             print(f"x: {x.shape}, residual: {residual.shape}")
             x = torch.cat([x, residual], dim=1)
             x = self.layers[i](x)
             print(f"x: {x.shape}")
-        
+
         return x
 
 
@@ -78,15 +83,15 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         self.down_filters = down_filters
         self.down_block = DownBlock(down_filters, in_channels)
-        
+
         # the bottom-most conv block is different from the rest of the blocks
         # in that it doesn't contain a maxpool and upsamples without a residual connection
         self.bottom_conv = nn.Sequential(
-            ResBlock(down_filters[-1], down_filters[-1]*2), 
-            nn.ConvTranspose2d(down_filters[-1]*2, down_filters[-1], 2, stride=2)
+            ResBlock(down_filters[-1], down_filters[-1] * 2),
+            nn.ConvTranspose2d(down_filters[-1] * 2, down_filters[-1], 2, stride=2),
         )
-        
-        self.up_filters = [down_filters[-1]*2]
+
+        self.up_filters = [down_filters[-1] * 2]
         self.up_filters.extend(reversed(down_filters))
         self.up_block = UpBlock(self.up_filters)
 
