5f15f782b8f64fa92f903900492a36a2ac9c7b81
aryamanpandya99 <aryaman.pandya@tufts.edu>
2024-10-16 13:23:21 -0400

    in progress- diffusion training process seems to be broken

--- src/utils.py
+++ src/utils.py
@@ -4,11 +4,9 @@ import numpy as np
 def show_torch_image(img):
     # Check if the image has only one channel (grayscale)
     if img.shape[0] == 1:
-        img = img.squeeze(0)  # Remove the channel dimension
+        img = img.squeeze(0).cpu().numpy()
     else:
-        img = img.permute(1, 2, 0)
-    
-    img = img.numpy().astype(np.uint8)
+        img = img.permute(1, 2, 0).cpu().numpy()
 
     plt.imshow(img, cmap='gray' if len(img.shape) == 2 else None)
     plt.axis('off')
