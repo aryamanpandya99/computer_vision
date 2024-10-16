a7124075fac244d41b4941ee78af64bb35ae1e24
aryamanpandya99 <aryaman.pandya@tufts.edu>
2024-10-15 23:12:08 -0400

    fixed sampler

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 def show_torch_image(img):
-    img = img.permute(1, 2, 0)
+    # Check if the image has only one channel (grayscale)
+    if img.shape[0] == 1:
+        img = img.squeeze(0)  # Remove the channel dimension
+    else:
+        img = img.permute(1, 2, 0)
+    
     img = img.numpy().astype(np.uint8)
 
-    plt.imshow(img)
+    plt.imshow(img, cmap='gray' if len(img.shape) == 2 else None)
     plt.axis('off')
     plt.show()
